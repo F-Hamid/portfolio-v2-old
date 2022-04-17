13f49c09ba319ae014cb9667dbb67f143633eaa8
F-Hamid <hamid.fateh81@gmail.com>
2022-04-17 01:11:04 +0000

    Set home backgroud

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./sass/main.scss";
+import "./sass/pages/projects.scss";
+// import "./sass/pages/home.scss";
 import Home from "./components/Home";
 // import Projects from "./components/Projects";
 // import About from "./components/About";
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
+import HomeBackground from "./layouts/background";
+import "./../sass/pages/home.scss";
 
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <section className="section">
+      <HomeBackground />
+      <h1>
+        Hello <br></br>
+        <h6> This is a text for exemple</h6>
+      </h1>
+    </section>
+  );
 };
 
 export default Home;
