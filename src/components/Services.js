ca566e18b84d982fdf2d015cd045d65c7baa7f4e
F-Hamid <hamid.fateh81@gmail.com>
2023-05-07 12:41:43 +0100

    circles setup

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,14 +1,30 @@
 import "../sass/pages/services.scss";
+
 import Circle from "../components/layouts/ServicesCircle";
+import design from "../images/illustration.png";
+import code from "../images/coding.png";
+import seo from "../images/analyzing.png";
+import deploy from "../images/startup.png";
+import update from "../images/maintenance.png";
 
 const Services = () => {
   return (
     <section className="services" id="services">
-      <Circle title="Design" parallax="parallax-1" />
-      <Circle title="Coding" />
-      <Circle title="SEO/Analyze" />
-      <Circle title="Deploy" />
-      <Circle title="Update/Maintenance" />
+      <div className="header ">
+        <h2 className="header-heading shine-text">SERVICES</h2>
+        <hr className="header-bar" />
+        <p className="header-text">
+          My work reflects real life web applications, various skills and
+          problem solving. To explore even more, check out the links below.
+        </p>
+      </div>
+      <div className="circles-container">
+        <Circle title="Design" icon={design} />
+        <Circle title="Coding" icon={code} />
+        <Circle title="SEO/Analyze" icon={seo} />
+        <Circle title="Deploy" icon={deploy} />
+        <Circle title="Update/Maintenance" icon={update} />
+      </div>
     </section>
   );
 };
--- src/components/layouts/ServicesCircle.js
+++ src/components/layouts/ServicesCircle.js
@@ -1,7 +1,15 @@
 import "../../sass/layouts/servicesCircle.scss";
 
-const ServicesCircle = ({ title, parallax }) => {
-  return <div className={`circle ${parallax}`}>{title}</div>;
+const ServicesCircle = ({ title, icon, color }) => {
+  return (
+    <section className="circle-container">
+      <div className={`circle  ${color}`}>
+        <img src={icon} className="circle-image" alt="" />
+      </div>
+
+      <h2 className="title">{title}</h2>
+    </section>
+  );
 };
 
 export default ServicesCircle;
