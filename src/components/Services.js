7b05b2894a5da941133f75ee1f17833705936eb7
F-Hamid <hamid.fateh81@gmail.com>
2023-05-07 13:35:29 +0100

    circles title

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,6 +1,7 @@
 import "../sass/pages/services.scss";
 
 import Circle from "../components/layouts/ServicesCircle";
+import shapes from "../images/shapes.png";
 import design from "../images/illustration.png";
 import code from "../images/coding.png";
 import seo from "../images/analyzing.png";
@@ -10,6 +11,7 @@ import update from "../images/maintenance.png";
 const Services = () => {
   return (
     <section className="services" id="services">
+      <img src={shapes} alt="" className=" services-bg " />
       <div className="header ">
         <h2 className="header-heading shine-text">SERVICES</h2>
         <hr className="header-bar" />
@@ -19,11 +21,11 @@ const Services = () => {
         </p>
       </div>
       <div className="circles-container">
-        <Circle title="Design" icon={design} />
-        <Circle title="Coding" icon={code} />
-        <Circle title="SEO/Analyze" icon={seo} />
-        <Circle title="Deploy" icon={deploy} />
-        <Circle title="Update/Maintenance" icon={update} />
+        <Circle title="Design" icon={design} color="color-design" />
+        <Circle title="Coding" icon={code} color="color-code" />
+        <Circle title="SEO/Analyze" icon={seo} color="color-seo" />
+        <Circle title="Deploy" icon={deploy} color="color-deploy" />
+        <Circle title="Update/Maintenance" icon={update} color="color-update" />
       </div>
     </section>
   );
